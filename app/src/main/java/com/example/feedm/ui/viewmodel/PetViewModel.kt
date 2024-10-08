5c6d805ce4c3b67a52455b72a6bc796bddcf5436
RedWorm <redworm.contact@gmail.com>
2024-10-08 21:12:20 +0200

    Se arregló el error del PetViewModel.( Se cambio su constructor con parametro Context, por un constructor sin parametros para que no diera error su inicialización.

--- app/src/main/java/com/example/feedm/ui/viewmodel/PetViewModel.kt
+++ app/src/main/java/com/example/feedm/ui/viewmodel/PetViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.feedm.ui.viewmodel
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.feedm.data.model.PetModel
 import com.example.feedm.domain.GetPets
 
-class PetViewModel(context: Context): ViewModel(){
+class PetViewModel: ViewModel(){
 
     val pets = MutableLiveData<List<PetModel>>()
-    val getPetsUseCase = GetPets(context)
 
-    fun onCreate(){
-        pets.value = getPetsUseCase.invoke()
+
+    @SuppressLint("NullSafeMutableLiveData")
+    fun onCreate(context: Context) {
+        val getPetsUseCase = GetPets(context)
+        val result = getPetsUseCase()
+        if (!result.isNullOrEmpty()) {
+            pets.value = result
+        }
     }
 }
