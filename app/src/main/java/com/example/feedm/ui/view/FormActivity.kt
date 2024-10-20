69ba33423aca8207e91552a5d329e7b0cfb755ab
RedWorm <redworm.contact@gmail.com>
2024-10-20 03:19:41 +0200

    Se implemento el uso de binding para tener un codigo mas limpio y legible

--- app/src/main/java/com/example/feedm/ui/view/FormActivity.kt
+++ app/src/main/java/com/example/feedm/ui/view/FormActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.feedm.R
 import com.example.feedm.data.model.PetModel
+import com.example.feedm.databinding.ActivityFormBinding
 import com.example.feedm.ui.viewmodel.PetViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -20,15 +21,6 @@ import javax.inject.Inject
 class FormActivity : AppCompatActivity() {
 
     private val petViewModel: PetViewModel by viewModels()
-    // Declaración de vistas y variables de formulario
-    private lateinit var faEditTextPetName: EditText
-    private lateinit var faTextInputAlergias: EditText
-    private lateinit var faRgEsterilizado: RadioGroup
-    private lateinit var faArraySpinners: ArrayList<Spinner>
-    private lateinit var faArrayAdapter: ArrayList<Adapter>
-    private lateinit var faImgSelectAnimal: ImageView
-    private lateinit var txtWeight: TextView
-    private lateinit var sbWeight: SeekBar
 
     // Variables de datos
     private var id = 0
@@ -43,13 +35,16 @@ class FormActivity : AppCompatActivity() {
     private lateinit var alergia: String
     private lateinit var query: String
 
+    private lateinit var binding: ActivityFormBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         petViewModel.onCreate()
+        binding = ActivityFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -59,11 +54,10 @@ class FormActivity : AppCompatActivity() {
         innitFormViews()
 
         // Botón para confirmar el formulario
-        val faBtnCommit: Button = findViewById(R.id.ma_btnCommit)
         val intent = Intent(this, PetsActivity::class.java)
 
         // Listener para el botón de confirmar
-        faBtnCommit.setOnClickListener {
+        binding.faBtnCommit.setOnClickListener {
             if (recogerDatos()) {
                 // Crear y guardar una nueva mascota
                 val petModel = PetModel(id, animal, nombre, edad, peso, sexo, esterilizado, actividad, objetivo, alergia, query)
@@ -76,29 +70,12 @@ class FormActivity : AppCompatActivity() {
         }
 
         // Botón de regreso
-        val faBtnBack: ImageButton = findViewById(R.id.fa_btnBack)
-        faBtnBack.setOnClickListener { goBack() }
+        binding.faBtnBack.setOnClickListener { goBack() }
     }
 
     // Inicializa y configura todas las vistas del formulario
     private fun innitFormViews() {
-        sbWeight = findViewById(R.id.fa_sbWeight)
-        txtWeight = findViewById(R.id.fa_txtWeight)
-        animal = "dog"  // Valor por defecto del animal
-        faImgSelectAnimal = findViewById(R.id.fa_imgSelectAnimal)
-
-        // Spinners para seleccionar datos del formulario
-        faArraySpinners = arrayListOf(
-            findViewById(R.id.fa_SpinnerEdad),
-            findViewById(R.id.fa_SpinnerSexo),
-            findViewById(R.id.fa_SpinnerActividad),
-            findViewById(R.id.fa_SpinnerObjetivo)
-        )
-
-        // Elementos de nombre, alergias y esterilización
-        faRgEsterilizado = findViewById(R.id.fa_rgEsterilizado)
-        faEditTextPetName = findViewById(R.id.fa_etPetName)
-        faTextInputAlergias = findViewById(R.id.fa_TextInputAlergias)
+        binding.faImgSelectAnimal.setOnClickListener { animal = changeForm(animal) }
 
         // Configurar vistas del formulario
         setUpFormViews()
@@ -110,40 +87,38 @@ class FormActivity : AppCompatActivity() {
         var weightAdjusted: Int
 
         // Listener para el SeekBar del peso
-        sbWeight.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        binding.faSbWeight.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                weightAdjusted = (sbWeight.progress / 5) * 5
-                sbWeight.progress = weightAdjusted
+                weightAdjusted = (binding.faSbWeight.progress / 5) * 5
+                binding.faSbWeight.progress = weightAdjusted
                 stringForTxtWeight = "${weightAdjusted / 10.0} Kg"
-                txtWeight.text = stringForTxtWeight
+                binding.faTxtWeight.text = stringForTxtWeight
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
                 // Restablecer el color al tocar el SeekBar
-                sbWeight.thumbTintList = ContextCompat.getColorStateList(baseContext,
-                    R.color.tintilist_seekbar_standard
-                )
-                sbWeight.progressBackgroundTintList = ContextCompat.getColorStateList(baseContext,
-                    R.color.tintlist_standard
-                )
-                sbWeight.progressTintList = ContextCompat.getColorStateList(baseContext,
-                    R.color.tintilist_seekbar_standard
-                )
-                txtWeight.setTextColor(resources.getColor(R.color.black, null))
+                binding.faSbWeight.thumbTintList = ContextCompat.getColorStateList(baseContext, R.color.tintilist_seekbar_standard)
+                binding.faSbWeight.progressBackgroundTintList = ContextCompat.getColorStateList(baseContext, R.color.tintlist_standard)
+                binding.faSbWeight.progressTintList = ContextCompat.getColorStateList(baseContext, R.color.tintilist_seekbar_standard)
+                binding.faTxtWeight.setTextColor(resources.getColor(R.color.black, null))
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
-        // Listener para cambiar el tipo de animal (gato/perro)
-        faImgSelectAnimal.setOnClickListener { animal = changeForm(animal) }
-
         // Adaptadores para los spinners
-        faArrayAdapter = arrayListOf(
+        val faArrayAdapter = arrayListOf(
             ArrayAdapter(this, android.R.layout.simple_spinner_item, resources.getStringArray(R.array.fa_arraySpinnerEdad)),
             ArrayAdapter(this, android.R.layout.simple_spinner_item, resources.getStringArray(R.array.fa_arraySpinnerSexo)),
             ArrayAdapter(this, android.R.layout.simple_spinner_item, resources.getStringArray(R.array.fa_arraySpinnerActividadFisica)),
             ArrayAdapter(this, android.R.layout.simple_spinner_item, resources.getStringArray(R.array.fa_arraySpinnerObjetivo))
+        )
+
+        val faArraySpinners = arrayListOf(
+            binding.faSpinnerEdad,
+            binding.faSpinnerSexo,
+            binding.faSpinnerActividad,
+            binding.faSpinnerObjetivo
         )
 
         // Asignar adaptadores a los spinners
@@ -152,12 +127,10 @@ class FormActivity : AppCompatActivity() {
         }
 
         // Restablecer el color al seleccionar un RadioButton del RadioGroup
-        faRgEsterilizado.setOnCheckedChangeListener { radioGroup, _ ->
-            for(i in 0 until faRgEsterilizado.childCount){
-                val radioButton = faRgEsterilizado.getChildAt(i) as RadioButton
-                radioButton.buttonTintList = ContextCompat.getColorStateList(this,
-                    R.color.tintlist_standard
-                )
+        binding.faRgEsterilizado.setOnCheckedChangeListener { _, _ ->
+            for (i in 0 until binding.faRgEsterilizado.childCount) {
+                val radioButton = binding.faRgEsterilizado.getChildAt(i) as RadioButton
+                radioButton.buttonTintList = ContextCompat.getColorStateList(this, R.color.tintlist_standard)
             }
         }
     }
@@ -165,14 +138,12 @@ class FormActivity : AppCompatActivity() {
     // Cambia el formulario entre perro y gato, ajustando la imagen y el peso
     private fun changeForm(animalIs: String): String {
         return if (animalIs == "dog") {
-            faImgSelectAnimal.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.gato))
-            sbWeight.max = 250
+            binding.faImgSelectAnimal.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.gato))
+            binding.faSbWeight.max = 250
             "cat"
         } else {
-            faImgSelectAnimal.setImageDrawable(AppCompatResources.getDrawable(this,
-                R.drawable.img_dog_illustration
-            ))
-            sbWeight.max = 800
+            binding.faImgSelectAnimal.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.img_dog_illustration))
+            binding.faSbWeight.max = 800
             "dog"
         }
     }
@@ -185,49 +156,45 @@ class FormActivity : AppCompatActivity() {
         }
 
         // Validar nombre de la mascota
-        if (faEditTextPetName.text.isEmpty()) {
-            faEditTextPetName.error = "This can not be empty"
+        if (binding.faEtPetName.text.isEmpty()) {
+            binding.faEtPetName.error = "This can not be empty"
             return false
         }
 
         // Asignar valores de los campos
-        nombre = faEditTextPetName.text.toString()
-        alergia = faTextInputAlergias.text.toString()
-        edad = faArraySpinners[0].selectedItem.toString()
+        nombre = binding.faEtPetName.text.toString()
+        alergia = binding.faTextInputAlergias.text.toString()
+        edad = binding.faSpinnerEdad.selectedItem.toString()
 
         // Validar el peso
-        if (sbWeight.progress == 0) {
-            sbWeight.thumbTintList = ContextCompat.getColorStateList(this, R.color.tintlist_error)
-            sbWeight.progressBackgroundTintList = ContextCompat.getColorStateList(this,
-                R.color.tintlist_error
-            )
-            txtWeight.setTextColor(resources.getColor(R.color.red, null))
+        if (binding.faSbWeight.progress == 0) {
+            binding.faSbWeight.thumbTintList = ContextCompat.getColorStateList(this, R.color.tintlist_error)
+            binding.faSbWeight.progressBackgroundTintList = ContextCompat.getColorStateList(this, R.color.tintlist_error)
+            binding.faTxtWeight.setTextColor(resources.getColor(R.color.red, null))
             return false
         }
 
         // Asignar más valores del formulario
-        peso = sbWeight.progress / 10.0
-        sexo = faArraySpinners[1].selectedItem.toString()
+        peso = binding.faSbWeight.progress / 10.0
+        sexo = binding.faSpinnerSexo.selectedItem.toString()
 
         // Validar si se seleccionó la opción de esterilización
-        esterilizado = when (faRgEsterilizado.checkedRadioButtonId) {
+        esterilizado = when (binding.faRgEsterilizado.checkedRadioButtonId) {
             R.id.fa_rbEsterilizado1 -> "Si"
             R.id.fa_rbEsterilizado2 -> "No"
             else -> ""
         }
 
         if (esterilizado.isEmpty()) {
-            for(i in 0 until faRgEsterilizado.childCount){
-                val radioButton = faRgEsterilizado.getChildAt(i) as RadioButton
-                radioButton.buttonTintList = ContextCompat.getColorStateList(this,
-                    R.color.tintlist_error
-                )
+            for (i in 0 until binding.faRgEsterilizado.childCount) {
+                val radioButton = binding.faRgEsterilizado.getChildAt(i) as RadioButton
+                radioButton.buttonTintList = ContextCompat.getColorStateList(this, R.color.tintlist_error)
             }
             return false
         }
 
-        actividad = faArraySpinners[2].selectedItem.toString()
-        objetivo = faArraySpinners[3].selectedItem.toString()
+        actividad = binding.faSpinnerActividad.selectedItem.toString()
+        objetivo = binding.faSpinnerObjetivo.selectedItem.toString()
 
         // Crear consulta para el registro
         query = this.resources.getString(R.string.query, animal, sexo, edad, peso)
--- app/src/main/java/com/example/feedm/ui/view/FragmentEditPet.kt
+++ app/src/main/java/com/example/feedm/ui/view/FragmentEditPet.kt
@@ -1,164 +1,137 @@
 package com.example.feedm.ui.view
 
-
 import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.RadioGroup
 import android.widget.SeekBar
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
-import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.feedm.R
 import com.example.feedm.data.model.PetModel
-
+import com.example.feedm.databinding.FragmentFormEditPetBinding
 import com.example.feedm.ui.viewmodel.PetViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
 import kotlin.math.roundToInt
 
-
 @AndroidEntryPoint
-class FragmentEditPet  : Fragment() {
+class FragmentEditPet : Fragment() {
+
     private lateinit var pet: PetModel
     private var pos = -1
     private val bundle = Bundle()
-    private lateinit var fragmenteditpetTxtName: TextView
-    private lateinit var fragmenteditpetTxtWeight: TextView
-    private lateinit var fragmenteditpetImgpet: ImageView
-    private lateinit var fragmenteditpetEtAllergies: EditText
-    private lateinit var fragmenteditpetSbWeight: SeekBar
-    private lateinit var fragmenteditpetRgSterilized: RadioGroup
-    private lateinit var fragmenteditpetBtncancel: Button
-    private lateinit var fragmenteditpetBtnapply: Button
-    private val petViewModel: PetViewModel by viewModels ()
-
+    private val petViewModel: PetViewModel by viewModels()
+    private var _binding: FragmentFormEditPetBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pos = arguments?.getInt(BUNDLE_POS)!!
+        pos = arguments?.getInt(BUNDLE_POS) ?: -1
         petViewModel.onCreate()
         petViewModel.pets.observe(this, Observer { pet = it[pos] })
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_form_edit_pet, container, false)
-
+        _binding = FragmentFormEditPetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
-    override fun onResume(){
+    override fun onResume() {
         super.onResume()
         val paLytConstraint = requireActivity().findViewById<ConstraintLayout>(R.id.pa_lytRecyclerView)
         val blurEffect = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            RenderEffect.createBlurEffect(20f,20f,Shader.TileMode.CLAMP)
+            RenderEffect.createBlurEffect(20f, 20f, Shader.TileMode.CLAMP)
         } else {
             TODO("VERSION.SDK_INT < S")
         }
-        Log.i("step3","onResume()")
+        Log.i("step3", "onResume()")
         paLytConstraint.setRenderEffect(blurEffect)
         initViews()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("step5","onDestroy()")
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        Log.i("step5", "onDestroyView()")
         val paLytConstraint = requireActivity().findViewById<ConstraintLayout>(R.id.pa_lytRecyclerView)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             paLytConstraint.setRenderEffect(null)
         }
     }
 
-
-    private fun initViews(){
-        fragmenteditpetTxtName = view?.findViewById(R.id.fragmentEditPet_txtName)!!
-        fragmenteditpetTxtWeight = view?.findViewById(R.id.fragmentEditPet_txtWeight)!!
-        fragmenteditpetImgpet = view?.findViewById(R.id.fragmentEditPet_ImageView)!!
-        fragmenteditpetEtAllergies = view?.findViewById(R.id.fragmentEditPet_TextInputAlergias)!!
-        fragmenteditpetSbWeight = view?.findViewById(R.id.fragmentEditPet_sbWeight)!!
-        fragmenteditpetRgSterilized = view?.findViewById(R.id.fragmentEditPet_rgEsterilizado)!!
-        fragmenteditpetBtncancel = view?.findViewById(R.id.fragmentEditPet_btnCancelar)!!
-        fragmenteditpetBtnapply = view?.findViewById(R.id.fragmentEditPet_btnConfirmar)!!
-        fragmenteditpetTxtName.text = pet.nombre
-        if(pet.animal.equals("dog")){
-            fragmenteditpetImgpet.setImageDrawable(ResourcesCompat.getDrawable(resources,
-                R.drawable.img_dog_illustration,null))
-        }
-        else {
-            fragmenteditpetImgpet.setImageDrawable(ResourcesCompat.getDrawable(resources,
-                R.drawable.gato,null))
-        }
-
-        fragmenteditpetEtAllergies.setText(pet.alergia)
-        if(pet.esterilizado.equals("No")){
-            fragmenteditpetRgSterilized.check(R.id.fragmentEditPet_rbSterilized2)
-        }
-        else {
-            fragmenteditpetRgSterilized.check(R.id.fragmentEditPet_rbSterilized1)
-        }
-        fragmenteditpetTxtWeight.text = String.format(null,"%.1f Kg", pet.peso)
-        fragmenteditpetSbWeight.progress=pet.peso!!.times(10).roundToInt()
-
-        var numberForTxtWeight : Double
-        var weightAdjusted : Int
-        fragmenteditpetSbWeight.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                val weight = fragmenteditpetSbWeight.progress
-                weightAdjusted = (weight / 5) * 5
-                numberForTxtWeight= weightAdjusted / 10.0
-                fragmenteditpetTxtWeight.text = String.format(null,"%.1f Kg", numberForTxtWeight)
+    private fun initViews() {
+        with(binding) {
+            fragmentEditPetTxtName.text = pet.nombre
+            if (pet.animal == "dog") {
+                fragmentEditPetImageView.setImageDrawable(
+                    ResourcesCompat.getDrawable(resources, R.drawable.img_dog_illustration, null)
+                )
+            } else {
+                fragmentEditPetImageView.setImageDrawable(
+                    ResourcesCompat.getDrawable(resources, R.drawable.gato, null)
+                )
             }
 
-            override fun onStartTrackingTouch(p0: SeekBar?) {
+            fragmentEditPetTextInputAlergias.setText(pet.alergia)
+            if (pet.esterilizado == "No") {
+                fragmentEditPetRgEsterilizado.check(R.id.fragmentEditPet_rbSterilized2)
+            } else {
+                fragmentEditPetRgEsterilizado.check(R.id.fragmentEditPet_rbSterilized1)
+            }
+            fragmentEditPetTxtWeight.text = String.format(null, "%.1f Kg", pet.peso)
+            fragmentEditPetSbWeight.progress = pet.peso?.times(10)?.roundToInt() ?: 0
 
+            fragmentEditPetSbWeight.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    val weightAdjusted = (fragmentEditPetSbWeight.progress / 5) * 5
+                    val numberForTxtWeight = weightAdjusted / 10.0
+                    fragmentEditPetTxtWeight.text = String.format(null, "%.1f Kg", numberForTxtWeight)
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {}
+            })
+
+            fragmentEditPetBtnCancelar.setOnClickListener {
+                bundle.putBoolean("finished", true)
+                parentFragmentManager.setFragmentResult("result", bundle)
             }
 
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-
-            }
-
-        })
-        fragmenteditpetBtncancel.setOnClickListener{
-            bundle.putBoolean("finished", true)
-            parentFragmentManager.setFragmentResult("result", bundle)
-        }
-
-        fragmenteditpetBtnapply.setOnClickListener{
+            fragmentEditPetBtnConfirmar.setOnClickListener {
                 commitEditPet()
+            }
         }
     }
 
-
-    private fun commitEditPet(){
-        Log.i("step4","commitEditPet()")
-            pet.alergia = fragmenteditpetEtAllergies.text.toString()
-            pet.peso = fragmenteditpetSbWeight.progress.div(10).toDouble()
-            when(fragmenteditpetRgSterilized.checkedRadioButtonId){
+    private fun commitEditPet() {
+        Log.i("step4", "commitEditPet()")
+        with(binding) {
+            pet.alergia = fragmentEditPetTextInputAlergias.text.toString()
+            pet.peso = fragmentEditPetSbWeight.progress.div(10).toDouble()
+            when (fragmentEditPetRgEsterilizado.checkedRadioButtonId) {
                 R.id.fragmentEditPet_rbSterilized1 -> pet.esterilizado = "Si"
                 R.id.fragmentEditPet_rbSterilized2 -> pet.esterilizado = "No"
+            }
         }
-        petViewModel.editPet(pet,pos)
+        petViewModel.editPet(pet, pos)
         bundle.putBoolean("finished", true)
         parentFragmentManager.setFragmentResult("result", bundle)
-        Log.i("step3223","ENVIADO")
+        Log.i("step3223", "ENVIADO")
     }
 
-    companion object{
+    companion object {
         const val BUNDLE_POS = "-1"
     }
 }
