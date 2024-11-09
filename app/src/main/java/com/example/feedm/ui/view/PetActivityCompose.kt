00119ed842ccdb17d3c39aef28b1e3895148efbe
RedWorm <redworm.contact@gmail.com>
2024-11-09 12:37:34 +0100

    Se completo el gradle y se creo la actividad donde se migrara el petsActivity a jetpack compose

---
+++ app/src/main/java/com/example/feedm/ui/view/PetActivityCompose.kt
@@ -0,0 +1,36 @@
+package com.example.feedm.ui.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.feedm.ui.view.ui.theme.FeedmTheme
+
+class PetActivityCompose : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+
+@Composable
+fun initViews(name: String){
+    Text(text = name)
+}
+
+@Preview
+@Composable
+fun previewTxt(){
+    initViews("hola")
+}
