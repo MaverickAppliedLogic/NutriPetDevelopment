67bf2832164eb6d00ef897155211c996b0e35233
RedWorm <redworm.contact@gmail.com>
2024-11-19 08:48:15 +0100

    -Se creó un radiogroup y se implemento al formulario

--- app/src/main/java/com/example/feedm/ui/view/FromActivityCompose.kt
+++ app/src/main/java/com/example/feedm/ui/view/FromActivityCompose.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -14,30 +13,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.FloatingActionButtonElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -50,15 +43,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.feedm.R
 import com.example.feedm.ui.view.ui.theme.FeedmTheme
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FromActivityCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -92,21 +82,27 @@ fun FormScreen(modifier: Modifier = Modifier) {
             )
         }
         PetName()
+        val options = stringArrayResource(id = R.array.fa_arraySpinnerActividadFisica).toList()
+        var selectedOption by remember { mutableStateOf(options[0]) }
+        Text(text = stringResource(id = R.string.fa_txtSpinnerEsterilizado))
+        CustomRadioGroup(selectedOption = selectedOption,
+            onOptionSelected = {selectedOption = it},
+            options = options, modifier = Modifier.fillMaxWidth())
         Column(
             modifier = Modifier
                 .weight(1f)
                 .verticalScroll(scrollState)
         ) {
-            DropDownMenuPers(
+            CustomDropDownMenu(
                 options = stringArrayResource(id = R.array.fa_arraySpinnerEdad).toList(),
                 title = stringResource(id = R.string.ma_txtSpinnerEdad)
             )
-            SliderPers()
-            DropDownMenuPers(
+            CustomSlider()
+            CustomDropDownMenu(
                 options = stringArrayResource(id = R.array.fa_arraySpinnerActividadFisica).toList(),
                 title = stringResource(id = R.string.fa_txtSpinnerActividadFisica)
             )
-            DropDownMenuPers(
+            CustomDropDownMenu(
                 options = stringArrayResource(id = R.array.fa_arraySpinnerObjetivo).toList(),
                 title = stringResource(id = R.string.fa_txtSpinnerObjetivo)
             )
@@ -152,7 +148,7 @@ fun PetName(modifier: Modifier = Modifier) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DropDownMenuPers(options: List<String>, title: String) {
+fun CustomDropDownMenu(options: List<String>, title: String) {
     var expanded by remember { mutableStateOf(false) }
     var selectedOption by remember { mutableStateOf(options[0]) }
     ExposedDropdownMenuBox(expanded = expanded,
@@ -195,12 +191,12 @@ fun DropDownMenuPers(options: List<String>, title: String) {
 @Preview
 @Composable
 fun DropDownMenuPreview(modifier: Modifier = Modifier) {
-    DropDownMenuPers(options = listOf("option1", "option2", "option3"), title = "Example")
+    CustomDropDownMenu(options = listOf("option1", "option2", "option3"), title = "Example")
 }
 
 @Preview
 @Composable
-fun SliderPers(modifier: Modifier = Modifier){
+fun CustomSlider(modifier: Modifier = Modifier){
     var sliderPosition by remember { mutableStateOf(0f)}
     Column(modifier = modifier.padding(horizontal = 10.dp)) {
         Text(stringResource(id = R.string.fa_txtSpinnerPeso))
@@ -209,6 +205,24 @@ fun SliderPers(modifier: Modifier = Modifier){
             onValueChange = {sliderPosition = it},
             valueRange = 0f..0.85f,
             modifier = Modifier.padding(5.dp))
+    }
+}
+
+@Composable
+fun CustomRadioGroup(modifier: Modifier = Modifier,
+                     options: List<String>,
+                     selectedOption: String,
+                     onOptionSelected: (String) -> Unit){
+    Row(modifier = modifier.fillMaxWidth()){
+        options.forEach{ option ->
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.weight(0.3f),
+                horizontalArrangement = Arrangement.Center) {
+                RadioButton(selected = selectedOption == option, onClick = { onOptionSelected(option) })
+                Text(text = option)
+            }
+
+        }
     }
 }
 
