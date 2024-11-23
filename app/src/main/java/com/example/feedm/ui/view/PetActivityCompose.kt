6982f477a1fc364ca327363a63925e1f26234654
RedWorm <redworm.contact@gmail.com>
2024-11-23 08:24:23 +0100

    Se agregó la funcionalidad del FormActivityComopose casi al completo, falta la seleccion de mascota.

--- app/src/main/java/com/example/feedm/ui/view/PetActivityCompose.kt
+++ app/src/main/java/com/example/feedm/ui/view/PetActivityCompose.kt
@@ -4,19 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -25,10 +20,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -36,29 +28,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Observer
-import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.example.feedm.R
-import com.example.feedm.domain.AddPet
 import com.example.feedm.domain.model.Pet
 import com.example.feedm.ui.view.ui.theme.FeedmTheme
 import com.example.feedm.ui.viewmodel.PetViewModel
@@ -104,21 +81,21 @@ fun PetsScreen(petViewModel: PetViewModel, modifier: Modifier = Modifier) {
             .padding(top = 20.dp),
         contentAlignment = Alignment.BottomEnd
     ) {
-        Pets(pets, modifier = modifier.fillMaxSize(1f))
+        PetsList(pets, modifier = modifier.fillMaxSize(1f))
     }
 
 }
 
 
 @Composable
-fun Pets(pets: List<Pet>, modifier: Modifier = Modifier) {
+fun PetsList(pets: List<Pet>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier.fillMaxWidth()) {
-        items(items = pets) { pet -> Pet(pet) }
+        items(items = pets) { pet -> PetItem(pet) }
     }
 }
 
 @Composable
-fun Pet(pet: Pet, modifier: Modifier = Modifier) {
+fun PetItem(pet: Pet, modifier: Modifier = Modifier) {
     Card(
         onClick = { /*TODO*/ },
         modifier
@@ -188,7 +165,7 @@ fun PetScreenPreview(modifier: Modifier = Modifier) {
                     .padding(innerpadding),
                 contentAlignment = Alignment.BottomEnd
             ) {
-                Pets(pets, modifier = modifier.fillMaxSize(1f))
+                PetsList(pets, modifier = modifier.fillMaxSize(1f))
             }
         }
     }
