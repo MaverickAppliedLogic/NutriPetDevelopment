80c664877e9fde6243aa88edfb4632124cff0bae
RedWorm <redworm.contact@gmail.com>
2024-11-24 08:54:15 +0100

    Se finalizó de implementar el diseño del formactivity. Faltaria solo la validación de datos.

--- app/src/main/java/com/example/feedm/ui/view/ui/theme/Color.kt
+++ app/src/main/java/com/example/feedm/ui/view/ui/theme/Color.kt
@@ -5,6 +5,10 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val Orange = Color(0xFFF8B200)
+val OrangeSemiSoft = Color(0xBCF8B200)
+val OrangeSoft = Color(0x6DFF9900)
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
