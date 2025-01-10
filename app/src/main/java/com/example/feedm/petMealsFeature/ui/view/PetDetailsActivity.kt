5fb02588c8f1923d1e4c4863bf77547c6a1eb6a1
RedWorm <redworm.contact@gmail.com>
2025-01-10 22:44:51 +0100

    Elimina: el comentario TODO y los imports inutilizados de PetDetailsActivity

--- app/src/main/java/com/example/feedm/petMealsFeature/ui/view/PetDetailsActivity.kt
+++ app/src/main/java/com/example/feedm/petMealsFeature/ui/view/PetDetailsActivity.kt
@@ -1,7 +1,6 @@
 package com.example.feedm.petMealsFeature.ui.view
 
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,65 +25,46 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.feedm.R
 import com.example.feedm.core.domain.model.MealModel
 import com.example.feedm.core.domain.model.PetModel
 import com.example.feedm.petMealsFeature.domain.model.PetMealsModel
 import com.example.feedm.petMealsFeature.ui.viewmodel.PetMealViewModel
 import com.example.feedm.petsFeature.ui.view.EditPetActivity
-import com.example.feedm.petsFeature.ui.view.FormActivity
 import com.example.feedm.petsFeature.ui.view.PetsActivity
 import com.example.feedm.ui.view.theme.AlmostWhite
 import com.example.feedm.ui.view.theme.Orange
 import com.example.feedm.ui.view.theme.TailyCareTheme
 import dagger.hilt.android.AndroidEntryPoint
 import okhttp3.internal.format
-
-//TODO Al darle al botón de volver atrás asegurarse de que vuelva a `PetsActivity` y no a
-// `EditPetActivity`
 
 @AndroidEntryPoint
 class PetDetailsActivity : ComponentActivity() {
