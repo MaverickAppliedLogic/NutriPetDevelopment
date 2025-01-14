74de0cf361ff0b2354c677060b2262a1f60e1048
RedWorm <redworm.contact@gmail.com>
2025-01-14 19:56:22 +0100

    AÑADE: se implemento por completo la clase calculator tanto para perros como para gatos asi como los tes unitarios correspondientes

--- app/src/main/java/com/example/feedm/core/utils/CaloriesCalculator.kt
+++ app/src/main/java/com/example/feedm/core/utils/CaloriesCalculator.kt
@@ -6,9 +6,7 @@ import kotlin.math.pow
 interface CaloriesCalculator {
 
     fun calculateCalories(pet: PetModel): Double{
-        val b = calculateBaseRequirements(pet)
-        val v = calculateVariants(pet)
-        return b * v
+        return calculateBaseRequirements(pet) * calculateVariants(pet)
     }
 
     fun calculateBaseRequirements(pet: PetModel): Double
@@ -49,4 +47,37 @@ class CaloriesCalculatorDog : CaloriesCalculator {
         return variants
     }
 
+}
+
+class CaloriesCalculatorCat : CaloriesCalculator {
+
+    override fun calculateBaseRequirements(pet: PetModel): Double {
+        val weightDouble = pet.petWeight.toDouble()
+        val ageFactor = if(pet.age < 1.0f){ 60 }
+        else if (pet.age > 7.0f){ 50 }
+        else{ 55 }
+        return weightDouble * ageFactor
+    }
+
+    override fun calculateVariants(pet: PetModel): Double {
+
+        var variants = 1.0
+
+        variants += when(pet.activity){
+            "Alta" -> 0.15
+            "Media" -> 0.05
+            "Baja"  -> 0.0
+            else -> 0.1
+        }
+        when(pet.goal){
+            "Perdida de peso" -> variants -= 0.15
+            "Mantenimiento" -> variants -= 0.0
+            "Aumento de peso" -> variants += 0.15
+        }
+        if(pet.sterilized){
+            variants -= 0.10
+        }
+
+        return variants
+    }
 }
