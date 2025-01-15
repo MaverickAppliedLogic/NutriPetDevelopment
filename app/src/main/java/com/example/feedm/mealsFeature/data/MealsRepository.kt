3d109fc377d44f683f5445676a873a288f240c69
RedWorm <redworm.contact@gmail.com>
2025-01-15 18:02:17 +0100

    ADD: Se creo la estructura de carpetas de mealsFeature, su repositorio junto a sus testings unitarios y los casos de uso que se usaran en la actividad

---
+++ app/src/main/java/com/example/feedm/mealsFeature/data/MealsRepository.kt
@@ -0,0 +1,22 @@
+package com.example.feedm.mealsFeature.data
+
+import com.example.feedm.core.data.database.dao.MealDao
+import com.example.feedm.core.data.database.entities.toDatabase
+import com.example.feedm.core.domain.model.MealModel
+import com.example.feedm.core.domain.model.toDomain
+import javax.inject.Inject
+
+class MealsRepository @Inject constructor(private val mealsDao: MealDao) {
+
+    suspend fun getMealsByPetId(petId: Int): List<MealModel> {
+       return mealsDao.getMealsByPetId(petId).map { it.toDomain() }
+    }
+
+    suspend fun addMealForAPet(meal: MealModel) {
+        mealsDao.addMealForAPet(meal.toDatabase())
+    }
+
+    suspend fun deleteMealForAPet(mealId: Int) {
+        mealsDao.deleteMealForAPet(mealId)
+    }
+}
