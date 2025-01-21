04a19dbea350fb62960a14f7566154ffb375c549
RedWorm <redworm.contact@gmail.com>
2025-01-21 18:42:56 +0100

    WIP: Se comenzó a implementar la funcionalidad de la UI AddMealActivity

--- app/src/main/java/com/example/feedm/core/domain/model/FoodModel.kt
+++ app/src/main/java/com/example/feedm/core/domain/model/FoodModel.kt
@@ -1,3 +1,9 @@
 package com.example.feedm.core.domain.model
 
-data class FoodModel()
+data class FoodModel(
+    val foodId: Int = 0,
+    val foodName: String,
+    val brand: String = "noBrand",
+    val foodWeight: Float?,
+    val calories: Float
+)
--- app/src/main/java/com/example/feedm/mealsFeature/ui/MealsViewmodel.kt
+++ app/src/main/java/com/example/feedm/mealsFeature/ui/MealsViewmodel.kt
@@ -2,17 +2,20 @@ package com.example.feedm.mealsFeature.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.feedm.core.domain.model.MealModel
 import com.example.feedm.mealsFeature.domain.AddMeal
 import com.example.feedm.mealsFeature.domain.DeleteMeal
 import com.example.feedm.mealsFeature.domain.GetMeals
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class MealsViewmodel @Inject constructor(
     private val getMealsUseCase: GetMeals,
     private val addMealUseCase: AddMeal,
     private val deleteMealUseCase: DeleteMeal
-) {
+): ViewModel() {
 
     private val _meals = MutableLiveData<List<MealModel>>()
     val meals: LiveData<List<MealModel>> = _meals
