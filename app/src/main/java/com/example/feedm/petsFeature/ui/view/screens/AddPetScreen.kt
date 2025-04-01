ddd17cf2543bc7e7b3c660e78a513b86889229de
Marcelo da Silva <redworm.contact@gmail.com>
2025-04-01 11:39:40 +0200

    Feat add pet form (#28)
    * WIP: Refactor theme and UI components, implement AddPetScreen with improved design
    
    This commit refactors the application's theme and UI components, introducing a new color palette and improving the design of several components. It also implements the AddPetScreen with a new layout and structure.
    
    - Updated color palette: Replaced the old orange and purple-based theme with a new palette featuring neutral, primary, and secondary colors.
    - Refactored CustomRadioGroup: Updated the colors of the radio buttons to match the new theme.
    - Updated CustomSlider: Improved the slider's appearance to align with the new color scheme.
    - Refactored CustomTimePicker: Adjusted the colors and design of the time picker to integrate with the new theme.
    - Implemented AddPetScreen: Created a new AddPetScreen layout with a progress indicator, form fields, and bottom app bar.
    - Added AddCompanionScreen: Introduced AddCompanionScreen to house the main logic of adding a new pet, including a progress indicator and a form.
    - Created ProgressIndicator: Implemented a new ProgressIndicator for the AddPetScreen to show the user's progress.
    - Implemented Form: Added Form component in AddPetScreen for the input fields.
    - Created FormField: Implemented a new FormField component for the input fields in the Form.
    
    Modified files:
    - `app/src/main/java/com/example/feedm/core/ui/components/CustomRadioGroup.kt`
    - `app/src/main/java/com/example/feedm/core/ui/theme/Theme.kt`
    - `app/src/main/java/com/example/feedm/core/ui/theme/Color.kt`
    - `app/src/main/java/com/example/feedm/petsFeature/ui/view/screens/AddPetScreen.kt`
    - `app/src/main/java/com/example/feedm/core/ui/components/CustomSlider.kt`
    - `app/src/main/java/com/example/feedm/core/ui/components/CustomTimePicker.kt`
    
    * FEAT: Enhance Add Pet Screen with Progress Indicator and improved design
    
    This commit introduces a progress indicator to the Add Pet screen, along with several design improvements.
    
    - Implemented a progress indicator on the Add Pet screen. The user now can see how much is completed the form
    - Added a dog icon to the slider thumb for a visual enhancement.
    - Enhanced the design of the top section with a vertical gradient background.
    - Updated the text styles and colors for better readability and aesthetics.
    - Added new drawable resources for the dog icon.
    
    Modified files:
    - `app/src/main/java/com/example/feedm/petsFeature/ui/view/screens/AddPetScreen.kt`
    - `app/src/main/res/mipmap-hdpi/dog_icon.png`
    - `app/src/main/res/mipmap-mdpi/dog_icon.png`
    - `app/src/main/res/mipmap-xhdpi/dog_icon.png`
    - `app/src/main/res/mipmap-xxhdpi/dog_icon.png`
    - `app/src/main/res/mipmap-xxxhdpi/dog_icon.png`
    
    * FEAT: Enhance Add Pet Screen with Progress Indicator and improved design
    
    This commit introduces a progress indicator to the Add Pet screen, along with several design improvements.
    
    - Implemented a progress indicator on the Add Pet screen. The user now can see how much is completed the form
    - Added a dog icon to the slider thumb for a visual enhancement.
    - Enhanced the design of the top section with a vertical gradient background.
    - Updated the text styles and colors for better readability and aesthetics.
    - Added new drawable resources for the dog icon.
    
    Modified files:
    - `app/src/main/java/com/example/feedm/petsFeature/ui/view/screens/AddPetScreen.kt`
    - `app/src/main/res/mipmap-hdpi/dog_icon.png`
    - `app/src/main/res/mipmap-mdpi/dog_icon.png`
    - `app/src/main/res/mipmap-xhdpi/dog_icon.png`
    - `app/src/main/res/mipmap-xxhdpi/dog_icon.png`
    - `app/src/main/res/mipmap-xxxhdpi/dog_icon.png`
    
    * m

---
+++ app/src/main/java/com/example/feedm/petsFeature/ui/view/screens/AddPetScreen.kt
@@ -0,0 +1,189 @@
+package com.example.feedm.petsFeature.ui.view.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.feedm.R
+import com.example.feedm.core.ui.theme.Neutral
+import com.example.feedm.core.ui.theme.NeutralDark
+import com.example.feedm.core.ui.theme.NeutralLight
+import com.example.feedm.core.ui.theme.Primary
+import java.util.Locale
+
+
+@Preview
+@Composable
+fun AddPetScreen(navToBackStack: () -> Unit = {}) {
+
+    Scaffold(bottomBar = {
+        BottomAppBar(containerColor = NeutralLight) {
+            Row(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Transparent),
+
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    shape = RoundedCornerShape(5.dp),
+                    onClick = { /* Acción al presionar el botón */ },
+                ) {
+                    Text(text = "Añadir")
+                }
+            }
+        }
+    }, contentWindowInsets = WindowInsets.safeDrawing){
+        AddPetContent(modifier = Modifier.padding(it))
+    }
+}
+
+@Composable
+fun AddPetContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        ProgressIndicator(modifier = Modifier.weight(0.25f, true))
+        Form(modifier = Modifier.weight(0.75f, true))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProgressIndicator(modifier: Modifier = Modifier) {
+    var sliderValue by remember { mutableFloatStateOf(0.5f) }
+    val sliderText by remember {
+        derivedStateOf {
+            String.format(Locale.getDefault(), "%.0f/7", sliderValue * 10)
+        }
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(NeutralDark, Neutral)
+                )
+            )
+    ) {
+        Column {
+            Text(
+                text = "Nuevo Compañero",
+                style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+                color = Primary,
+                modifier = Modifier.padding(10.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f, true))
+            Text(
+                text = sliderText,
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                color = Primary,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            )
+            Slider(
+                value = sliderValue,
+                onValueChange = { sliderValue = it },
+                colors = SliderDefaults.colors(
+                    thumbColor = Primary,
+                    activeTrackColor = Primary,
+                    inactiveTrackColor = NeutralLight,
+                    activeTickColor = Primary,
+                    inactiveTickColor = NeutralLight
+                ),
+                thumb = {
+                    Icon(
+                        painter = painterResource(R.mipmap.dog_icon),
+                        contentDescription = null,
+                        tint = Primary,
+                        modifier = Modifier
+                            .size(ButtonDefaults.IconSize)
+                            .scale(1.5f)
+                    )
+                },
+                valueRange = 0f..0.7f,
+            )
+            Spacer(modifier = Modifier.weight(1f, true))
+        }
+
+    }
+}
+
+@Composable
+fun Form(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(NeutralLight)
+    ) {
+        FormField(label = "Nuevo Compañero", modifier = Modifier.weight(1f, true))
+        FormField(label = "Edad", modifier = Modifier.weight(1f, true))
+        FormField(label = "Sexo (Opcional)", modifier = Modifier.weight(1f, true))
+        FormField(label = "Peso", modifier = Modifier.weight(1f, true))
+        FormField(label = "Objetivo", modifier = Modifier.weight(1f, true))
+        FormField(label = "¿Está esterilizado? (Opcional)", modifier = Modifier.weight(1f, true))
+        FormField(label = "Actividad Física", modifier = Modifier.weight(1f, true))
+    }
+}
+
+@Composable
+fun FormField(
+    label: String,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit = {}
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 15.dp, horizontal = 10.dp)
+    ) {
+        Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
+        Spacer(modifier = Modifier.padding(8.dp))
+        Text(text = label, style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = null)
+
+    }
+    HorizontalDivider()
+}
+
