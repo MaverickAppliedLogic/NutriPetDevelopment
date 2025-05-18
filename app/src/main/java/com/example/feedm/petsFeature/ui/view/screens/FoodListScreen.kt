a7a756fde0d799ce7f8373a37a43f0b59f84176c
RedWorm <redworm.contact@gmail.com>
2025-05-18 11:36:14 +0200

    FEAT: Implement FoodListScreen UI with Card and FloatingActionButton
    This commit introduces the initial UI for the FoodListScreen. It features a LazyColumn to display food items as cards, each with a label and an image. Two FloatingActionButtons for "Edit" and "Add" actions are also included.
    
    - Implemented `FoodListScreen` composable with a Scaffold.
    - Created `FoodListContent` composable to hold the main layout, including a gradient background, a dropdown icon, and a LazyColumn for food items.
    - Added sample food items for preview purposes.
    - Implemented `FoodItemCard` composable to display individual food items with a label and an image.
    - Added two FloatingActionButtons for "Edit" and "Add" actions.
    - Styled components using MaterialTheme and custom colors.

--- app/src/main/java/com/example/feedm/petsFeature/ui/view/screens/FoodListScreen.kt
+++ app/src/main/java/com/example/feedm/petsFeature/ui/view/screens/FoodListScreen.kt
@@ -1,43 +1,152 @@
 package com.example.feedm.petsFeature.ui.view.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.feedm.R
+import com.example.feedm.core.ui.theme.Neutral
+import com.example.feedm.core.ui.theme.NeutralLight
+import com.example.feedm.core.ui.theme.Primary
+import com.example.feedm.core.ui.theme.PrimaryLight
 
 @Composable
-fun FoodListScreen(navToAddFood: () -> Unit,
-                   navToBackStack: () -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+fun FoodListScreen(
+    navToAddFood: () -> Unit,
+    navToBackStack: () -> Unit
+) {
+    Scaffold(
+        contentWindowInsets = WindowInsets.safeDrawing
     ) {
-        Spacer(modifier = Modifier.weight(1f, true))
-        Text(
-            text = "Food List Screen",
-            modifier = Modifier.padding(25.dp)
-        )
-        Spacer(modifier = Modifier.weight(1f, true))
-        Button(
-            onClick = {navToBackStack()},
-            modifier = Modifier.padding(16.dp)
+        FoodListContent(modifier = Modifier.padding(it))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FoodListContent(
+    modifier: Modifier = Modifier
+) {
+    val foodItems = listOf(
+        Pair("Snack", R.drawable.img_dog_illustration), // Reemplaza con tu recurso
+        Pair("Pienso", R.drawable.ic_tailycare)
+    )
+    val lazyListState = rememberLazyListState()
+
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(NeutralLight, Neutral)
+                    )
+                )
+                .padding(16.dp)
         ) {
-            Text(text = "Go Back")
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = "Dropdown",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            LazyColumn(
+                state = lazyListState,
+            ) {
+                items(foodItems.size) { item ->
+                    FoodItemCard(label = foodItems[item].first, imageRes = foodItems[item].second)
+                }
+
+            }
         }
-        Spacer(modifier = Modifier.weight(1f, true))
-        Button(
-            onClick =  navToAddFood,
-            modifier = Modifier.padding(16.dp)
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Text(text = "Add Food")
+            FloatingActionButton(
+                onClick = { /* Acción editar */ },
+                containerColor = Primary
+            ) {
+                Icon(Icons.Default.Edit, contentDescription = "Edit")
+            }
+
+            FloatingActionButton(
+                onClick = { /* Acción agregar */ },
+                containerColor = Primary
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
         }
-        Spacer(modifier = Modifier.weight(1f, true))
+    }
+}
+
+@Composable
+fun FoodItemCard(label: String, imageRes: Int) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = PrimaryLight),
+        modifier = Modifier
+            .padding(vertical = 16.dp)
+            .fillMaxWidth()
+            .height(80.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = label,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp),
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = label,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
