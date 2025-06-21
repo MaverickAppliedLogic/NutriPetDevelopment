f768cc6a059d4e8b3b17dfc97e06aaf4c3a6e1b7
RedWorm <redworm.contact@gmail.com>
2025-06-21 13:48:41 +0200

    FIX: Corrected version management and updated deployment target
    This commit addresses issues related to version management in the `VersionLocalStorage` and updates the IDE deployment target.
    
    - **Version Management:**
        - Modified `VersionLocalStorage` to default to version `0.5` if the JSON file is empty or cannot be parsed, ensuring a valid starting point for version checks.
        - Updated `DataModule` to initialize the `updateState` file with version `0.5` if it already exists but is empty. Previously, it only initialized if the file didn't exist.
    - **IDE Configuration:**
        - Updated the deployment target timestamp in `.idea/deploymentTargetSelector.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.maverickapps.nutripet"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
