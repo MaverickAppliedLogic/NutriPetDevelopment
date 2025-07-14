3a154959a5ec88cf4dbb47c302fc38118dd1fcc8
RedWorm <redworm.contact@gmail.com>
2025-07-14 11:59:59 +0200

    FEAT: Refine age selection and update calorie calculation for pet registration
    This commit enhances the pet registration process by refining the age selection and updating the calorie calculation logic. It also includes minor UI tweaks and changes to the development environment configuration.
    
    Key changes:
    - **Age Selection:**
        - Replaced the string array for age options with a numerical range (1-11) in `AgeField.kt`.
        - Updated `strings.xml` to reflect the new age selection format (e.g., "X años", "+10 años").
        - Ensured `PetDetails.kt` correctly displays the selected age, including the "+10 años" option.
    - **Calorie Calculation:**
        - Adjusted the calorie calculation variants for weight goals ("Bajar peso", "Mantener", "Subir peso") in `CaloriesCalculatorCat.kt`.
        - Made `calculateRequiredCalories` in `DashboardViewModel.kt` private as it's only used internally.
    - **UI & Text:**
        - Updated the `UpdatesNotesDialog.kt` with new release notes, focusing on improved age selection accuracy, correct age display in profiles, and automatic calorie recalculation upon data edit. Also highlighted the new meal notification feature.
    - **Development Environment:**
        - Changed the IDE deployment target selector to a physical device.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.maverickapps.nutripet"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.65"
+        versionCode = 6
+        versionName = "0.66"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
